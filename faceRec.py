ef83b632e93761207277ce886daccf76606b963e
Andrew <apeterson247@gmail.com>
2020-11-23 21:34:34 -0500

    Added mouth and face cascades.

--- faceRec.py
+++ faceRec.py
@@ -6,7 +6,8 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 # Create the haar cascade
-faceCascade = cv2.CascadeClassifier("profileface.xml")
+faceCascade = cv2.CascadeClassifier("cascades/frontal_face.xml")
+mouthCascade = cv2.CascadeClassifier("cascades/Mouth.xml")
 
 while(True):
 	# Capture frame-by-frame
@@ -23,11 +24,19 @@ while(True):
 		minSize=(10, 10)
 		#flags = cv2.CV_HAAR_SCALE_IMAGE
 	)
+	# Detect the Mouths in the picture
+	mouths = mouthCascade.detectMultiScale(
+		gray,
+		scaleFactor=1.3,
+		minNeighbors=8,
+		minSize=(6, 6)
+	)
 
 	# Draw a rectangle around the faces
 	for (x, y, w, h) in faces:
 		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
+	for (x, y, w, h) in mouths:
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
 	# Display the resulting frame
 	cv2.imshow('frame', frame)
